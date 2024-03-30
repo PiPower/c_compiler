60bd144c43f3ad58d08101b6a53cceb7c7278cc3
PiPower <gyros.pl@gmail.com>
2024-03-30 17:57:07 +0100

    4 basic math ops support for code gen

--- src/backend/code_generator.cpp
+++ src/backend/code_generator.cpp
@@ -1,14 +1,13 @@
 #include "../../include/backend/code_generator.hpp"
 
-const char* registers[16] = { "rax",  "rbx","rcx","rdx","rsi","rdi","rbp","rsp",
-                                "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15"};
-bool allocatedFlag[16];
+const char* registers[8] = {"r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15"};
+bool allocatedFlag[8];
 
 using namespace std;
 
-int allocateRegister()
+int allocateRegister(int base = 0 )
 {
-    for(int i=0 ; i < 16; i++)
+    for(int i=base ; i < 8; i++)
     {
         if(!allocatedFlag[i])
         {
@@ -21,10 +20,12 @@ int allocateRegister()
     return -1;
 }
 
+
+
 int load64Register(InstructionBuffer& buffer, AstNode* constant)
 {
     int reg = allocateRegister();
-    string instruction = "movq $" + to_string(constant->context.int_32) + ", %"  + registers[reg] + " \n";
+    string instruction = "\tmovq $" + to_string(constant->context.int_32) + ", %"  + registers[reg] + " \n";
     buffer.writeInstruction(instruction.c_str());
     return reg;
 }
@@ -33,16 +34,52 @@ int translate(InstructionBuffer& buffer, AstNode* root)
 {
     switch (root->nodeType)
     {
+    case NodeType::SUBTRACT:
+        {
+            int r1 = translate(buffer, root->children[0]);
+            int r2 = translate(buffer, root->children[1]);
+
+            string instruction = string{"\tsub %"} + registers[r2] + ", %"  + registers[r1] + " \n";
+            buffer.writeInstruction(instruction.c_str());
+            return r2;
+        }
+        break;
+    case NodeType::ADD:
+        {
+            int r1 = translate(buffer, root->children[0]);
+            int r2 = translate(buffer, root->children[1]);
+
+            string instruction = string{"\tadd %"} + registers[r2] + ", %"  + registers[r1] + " \n";
+            buffer.writeInstruction(instruction.c_str());
+            return r1;
+        }
+        break;
     case NodeType::MULTIPLY:
         {
             int r1 = translate(buffer, root->children[0]);
             int r2 = translate(buffer, root->children[1]);
 
-            string instruction = string{"imulq %"} + registers[r1] + ", %"  + registers[r2] + " \n";
+            string instruction = string{"\timulq %"} + registers[r2] + ", %"  + registers[r1] + " \n";
             buffer.writeInstruction(instruction.c_str());
-            return r2;
+            return r1;
         }
         break;
+    case NodeType::DIVIDE:
+        {
+            int r1 = translate(buffer, root->children[0]);
+            int r2 = translate(buffer, root->children[1]);
+            string instruction = "";
+
+            instruction += "\tmovq %" + string{registers[r1]}+ ", %rax\n";
+            instruction += "\txorq %rdx, %rdx\n";
+            instruction += string{"\tidivq %"} + registers[r2] + " \n";
+            instruction += "\tmovq %rax, %" + string{registers[r1]} + "\n";
+
+            buffer.writeInstruction(instruction.c_str());
+            return r1;
+        }
+        break;
+
     case NodeType::CONSTANT:
         return load64Register(buffer, root);
     default:
