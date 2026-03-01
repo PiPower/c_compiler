26549d9a6f5e3f487ed914280e7d4dda787c5278
PiPower <darthlord.pl@gmail.com>
2026-03-01 11:50:03 +0100

    added TokenType::new_line for new lines in source file

--- tests/test_lexer.cpp
+++ tests/test_lexer.cpp
@@ -17,7 +17,9 @@ int main()
     Token tok;
     size_t i = 0;
     do{
+
         lexer.Lex(&tok);
+
         TokenType::Type expectedToken = tokens[i];
         SourceLocation expectedLoc = locations[i];
 
@@ -38,11 +40,13 @@ std::vector<TokenType::Type> tokens = {
 // #include <stdio.h>
 TokenType::hash, TokenType::pp_include, TokenType::less,
 TokenType::identifier, TokenType::dot, TokenType::identifier,
-TokenType::greater,
+TokenType::greater, TokenType::new_line,
 
 // #define MAX 100
 TokenType::hash, TokenType::pp_define, TokenType::identifier,
-TokenType::numeric_literal,
+TokenType::numeric_literal, TokenType::new_line,
+
+TokenType::new_line,
 
 // typedef struct Point { int x, y; } Point_t;
 TokenType::kw_typedef, TokenType::kw_struct, TokenType::identifier,
@@ -50,12 +54,16 @@ TokenType::l_brace,
 TokenType::kw_int, TokenType::identifier, TokenType::comma,
 TokenType::identifier, TokenType::semicolon,
 TokenType::r_brace, TokenType::identifier, TokenType::semicolon,
+TokenType::new_line,
+
+TokenType::new_line,
 
 // int main(void) {
 TokenType::kw_int, TokenType::identifier,
 TokenType::l_parentheses, TokenType::kw_void,
 TokenType::r_parentheses,
 TokenType::l_brace,
+TokenType::new_line,
 
 // int a = 5, b = 10;
 TokenType::kw_int, TokenType::identifier,
@@ -63,6 +71,7 @@ TokenType::equal, TokenType::numeric_literal,
 TokenType::comma, TokenType::identifier,
 TokenType::equal, TokenType::numeric_literal,
 TokenType::semicolon,
+TokenType::new_line,
 
 // float f = 3.14, g = 2.71;
 TokenType::kw_float, TokenType::identifier,
@@ -70,38 +79,45 @@ TokenType::equal, TokenType::numeric_literal,
 TokenType::comma, TokenType::identifier,
 TokenType::equal, TokenType::numeric_literal,
 TokenType::semicolon,
+TokenType::new_line,
 
 // double d = 1e3;
 TokenType::kw_double, TokenType::identifier,
 TokenType::equal, TokenType::numeric_literal,
 TokenType::semicolon,
+TokenType::new_line,
 
 // long l = 1234567890L;
 TokenType::kw_long, TokenType::identifier,
 TokenType::equal, TokenType::numeric_literal,
 TokenType::semicolon,
+TokenType::new_line,
 
 // char c = 'Z';
 TokenType::kw_char, TokenType::identifier,
 TokenType::equal, TokenType::numeric_literal,
 TokenType::semicolon,
+TokenType::new_line,
 
 // _Bool flag = 1;
 TokenType::kw_bool, TokenType::identifier,
 TokenType::equal, TokenType::numeric_literal,
 TokenType::semicolon,
+TokenType::new_line,
 
 // const int ci = 42;
 TokenType::kw_const, TokenType::kw_int,
 TokenType::identifier, TokenType::equal,
 TokenType::numeric_literal, TokenType::semicolon,
+TokenType::new_line,
 
 // volatile int vi = 0;
 TokenType::kw_volatile, TokenType::kw_int,
 TokenType::identifier, TokenType::equal,
 TokenType::numeric_literal, TokenType::semicolon,
+TokenType::new_line,
 
-// if (a < b && b > 0 || !flag)
+// if (a < b && b > 0 || !flag) {
 TokenType::kw_if, TokenType::l_parentheses,
 TokenType::identifier, TokenType::less,
 TokenType::identifier, TokenType::double_ampersand,
@@ -110,6 +126,7 @@ TokenType::numeric_literal, TokenType::double_pipe,
 TokenType::bang, TokenType::identifier,
 TokenType::r_parentheses,
 TokenType::l_brace,
+TokenType::new_line,
 
 // a += 1; b -= 1; c++; d--;
 TokenType::identifier, TokenType::plus_equal,
@@ -120,11 +137,14 @@ TokenType::identifier, TokenType::plus_plus,
 TokenType::semicolon,
 TokenType::identifier, TokenType::minus_minus,
 TokenType::semicolon,
+TokenType::new_line,
 
 TokenType::r_brace,
+TokenType::new_line,
 
 // else {
 TokenType::kw_else, TokenType::l_brace,
+TokenType::new_line,
 
 // a *= 2; b /= 2; f *= g; g /= f;
 TokenType::identifier, TokenType::star_equal,
@@ -135,41 +155,51 @@ TokenType::identifier, TokenType::star_equal,
 TokenType::identifier, TokenType::semicolon,
 TokenType::identifier, TokenType::slash_equal,
 TokenType::identifier, TokenType::semicolon,
+TokenType::new_line,
 
 TokenType::r_brace,
+TokenType::new_line,
 
 // switch(a) {
 TokenType::kw_switch, TokenType::l_parentheses,
 TokenType::identifier, TokenType::r_parentheses,
 TokenType::l_brace,
+TokenType::new_line,
 
 // case 1: break;
 TokenType::kw_case, TokenType::numeric_literal,
 TokenType::colon,
 TokenType::kw_break, TokenType::semicolon,
+TokenType::new_line,
 
-// default:
+// default: ;
 TokenType::kw_default, TokenType::colon,
 TokenType::semicolon,
+TokenType::new_line,
 
 TokenType::r_brace,
+TokenType::new_line,
 
-// while(a < MAX)
+// while(a < MAX) {
 TokenType::kw_while, TokenType::l_parentheses,
 TokenType::identifier, TokenType::less,
 TokenType::identifier,
 TokenType::r_parentheses,
 TokenType::l_brace,
+TokenType::new_line,
 
 // a <<= 1;
 TokenType::identifier, TokenType::l_shift_equal,
 TokenType::numeric_literal, TokenType::semicolon,
+TokenType::new_line,
 
 // b >>= 1;
 TokenType::identifier, TokenType::r_shift_equal,
 TokenType::numeric_literal, TokenType::semicolon,
+TokenType::new_line,
 
 TokenType::r_brace,
+TokenType::new_line,
 
 // Point_t p = { .x = a, .y = b };
 TokenType::identifier, TokenType::identifier,
@@ -182,25 +212,34 @@ TokenType::dot, TokenType::identifier,
 TokenType::equal, TokenType::identifier,
 TokenType::r_brace,
 TokenType::semicolon,
+TokenType::new_line,
 
 // Point_t* ptr = &p;
 TokenType::identifier, TokenType::star,
 TokenType::identifier, TokenType::equal,
 TokenType::ampersand, TokenType::identifier,
 TokenType::semicolon,
+TokenType::new_line,
 
 // ptr->x = 0;
 TokenType::identifier, TokenType::arrow,
 TokenType::identifier, TokenType::equal,
 TokenType::numeric_literal,
 TokenType::semicolon,
+TokenType::new_line,
+
+// comment line
+TokenType::comment,
+TokenType::new_line,
 
 // return 0;
 TokenType::kw_return,
 TokenType::numeric_literal,
 TokenType::semicolon,
+TokenType::new_line,
 
 TokenType::r_brace,
+TokenType::new_line,
 
 TokenType::eof
 };
