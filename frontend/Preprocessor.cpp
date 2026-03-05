0ee9189b90eef027d36de86e96b0f3d019d5f340
PiPower <darthlord.pl@gmail.com>
2026-03-05 15:06:43 +0100

    initial layout for preprocessor

--- frontend/Preprocessor.cpp
+++ frontend/Preprocessor.cpp
@@ -10,7 +10,97 @@ lexer(mainFile, manager, opts), opts(opts)
 
 int32_t Preprocessor::Peek(Token* token)
 {
-    int32_t ret = lexer.Lex(token);
-    ret = lexer.Lex(token);
-    return ret;
+    int32_t ret = 0;
+    do
+    {
+        ret = lexer.Lex(token);
+    } while (token->type == TokenType::comment || token->type == TokenType::new_line);
+    
+    if(token->type == TokenType::hash)
+    {
+        ret = ExecuteDirective(token);
+    }
+
+    return 0;
+}
+
+int32_t Preprocessor::ExecuteDirective(Token *token)
+{
+    Token ppToken;
+    lexer.Lex(&ppToken);
+    switch (ppToken.type)
+    {
+    case TokenType::kw_if:      HandleIf(); break;
+    case TokenType::kw_else:    HandleElse();break;
+    case TokenType::pp_include: HandleInclude(); break;
+    case TokenType::pp_define:  HandleDefine(); break;
+    case TokenType::pp_ifdef:   HandleIfdef(); break;
+    case TokenType::pp_ifndef:  HandleIfndef(); break;
+    case TokenType::pp_elif:    HandleElif(); break;
+    case TokenType::pp_endif:   HandleEndif(); break;
+    case TokenType::pp_line:    HandleLine(); break;
+    case TokenType::pp_error:   HandleError(); break;
+    case TokenType::pp_pragma:  HandlePragma(); break;
+    default:
+        printf("Not expected preprocessing token \n");
+        exit(-1);
+        break;
+
+    }
+    return 0;
+}
+
+int32_t Preprocessor::HandleIf()
+{
+    return 0;
+}
+
+int32_t Preprocessor::HandleElse()
+{
+    return 0;
+}
+
+int32_t Preprocessor::HandleInclude()
+{
+    return 0;
+}
+
+int32_t Preprocessor::HandleDefine()
+{
+    return 0;
+}
+
+int32_t Preprocessor::HandleIfdef()
+{
+    return 0;
+}
+
+int32_t Preprocessor::HandleIfndef()
+{
+    return 0;
+}
+
+int32_t Preprocessor::HandleElif()
+{
+    return 0;
+}
+
+int32_t Preprocessor::HandleEndif()
+{
+    return 0;
+}
+
+int32_t Preprocessor::HandleLine()
+{
+    return 0;
+}
+
+int32_t Preprocessor::HandleError()
+{
+    return 0;
+}
+
+int32_t Preprocessor::HandlePragma()
+{
+    return 0;
 }
--- frontend/Preprocessor.hpp
+++ frontend/Preprocessor.hpp
@@ -5,7 +5,21 @@ struct Preprocessor
 {
     Preprocessor(FILE_STATE mainFile, FileManager* manager, const CompilationOpts* opts);
     int32_t Peek(Token* token);
-
+    int32_t ExecuteDirective(Token* token);
+    
+    int32_t HandleIf();
+    int32_t HandleElse();
+    int32_t HandleInclude();
+    int32_t HandleDefine();
+    int32_t HandleIfdef();
+    int32_t HandleIfndef();
+    int32_t HandleElif();
+    int32_t HandleEndif();
+    int32_t HandleLine();
+    int32_t HandleError();
+    int32_t HandlePragma();
     Lexer lexer;
     const CompilationOpts* opts;
+
+
 };
