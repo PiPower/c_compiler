bef5fe540078b21f28f6ce0122f195f51777b352
PiPower <darthlord.pl@gmail.com>
2026-07-08 20:24:18 +0200

    qoa changes to containers

--- run_container.sh
+++ run_container.sh
@@ -1,2 +1,6 @@
-docker run -v ./:/home/ubuntu/src/c_compiler --workdir /home/ubuntu/src/c_compiler --rm \
- simple_env sh -c "chmod +x examples/run_tests.sh && examples/run_tests.sh"
+docker run -v ./:/home/ubuntu/src/c_compiler \
+    --workdir /home/ubuntu/src/c_compiler \
+    --rm \
+    --user "$(id -u):$(id -g)" \
+    simple_env \
+    sh -c "chmod +x examples/run_tests.sh && examples/run_tests.sh"
