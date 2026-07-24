f7732fc6fe24ab31979f10aad13ce10cc7817dac
PiPower <darthlord.pl@gmail.com>
2026-07-24 20:55:27 +0200

    improved testing enviroment

--- examples/run_tests.sh
+++ examples/run_tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-rm -f test_logs.txt
+rm -f -r logs;
+mkdir logs
 rm -f .builtin_defines.hpp .c_search_paths.set
 mkdir -p build_cnt
 cd build_cnt
@@ -12,10 +13,16 @@ rm -f examples/*.ll
 for file in examples/*.c; do 
     base=$(basename "$file" .c); 
     echo "---------- Compiling $file ----------"; 
-    echo "---------- Compiling $file ----------" >> test_logs.txt;  
-    build_cnt/c_compiler $file  >> test_logs.txt
-    opt -passes=verify examples/$base.ll -disable-output &>> test_logs.txt
-    echo >> test_logs.txt; 
+    echo "---------- Compiling $file ----------">> logs/compiler_logs.txt 
+    build_cnt/c_compiler $file | sed $'s/^/\t/' >> logs/compiler_logs.txt ;
+    #pass for llvm correctness check
+    echo "---------- Checking $file ----------" >>  logs/grammar_logs.txt;  
+    opt -passes=verify examples/$base.ll -disable-output 2>&1 | sed $'s/^/\t/' >>  logs/grammar_logs.txt ;
+    echo >> logs/grammar_logs.txt;
+    #comparison check
+    echo "---------- Comparing $base.ll ----------" &>> logs/diff_logs.txt;  
+    diff examples/$base.ll  examples/verified/$base.ll 2>&1 | sed $'s/^/\t/' >> logs/diff_logs.txt;  
+    echo >> logs/diff_logs.txt;
 done
 
 rm .builtin_defines.hpp .c_search_paths.set
